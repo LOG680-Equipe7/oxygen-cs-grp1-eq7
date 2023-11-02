5d93ddc7bb8c3b77df8781bac9c1e65cf9ab3cef
PhilipTruong <90659451+PhilipTruong@users.noreply.github.com>
2023-11-02 02:34:26 -0400

    update workflow

--- test/test_main.py
+++ test/test_main.py
@@ -1,23 +1,11 @@
 """
 This module contains tests for the main application logic, primarily focusing on
-the interaction with the database and environment variable configuration.
+the interaction with environment variable configuration.
 """
 
-import datetime
 import pytest
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
 from src.main import Main
-from src.models import TemperatureLog
-
-
-# Database configuration
-DATABASE_URL = "postgresql://postgres:postgres@host.docker.internal:5432/mydb"
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
 
 
 # Fixtures
@@ -30,31 +18,6 @@ def mock_environ(monkeypatch):
     monkeypatch.setenv("TOKEN", "TEST_TOKEN")
     monkeypatch.setenv("T_MAX", "TEST_T_MAX")
     monkeypatch.setenv("T_MIN", "TEST_T_MIN")
-
-
-@pytest.fixture(scope="function")
-def db_session():
-    """
-    Provides a new database session for testing, ensuring that any changes
-    are rolled back afterwards.
-    """
-    Base.metadata.create_all(engine)
-    session = Session()
-    yield session
-    session.rollback()
-    session.close()
-
-
-@pytest.fixture(scope="function")
-def valid_temperature():
-    """
-    Provides a valid temperature log instance for testing.
-    """
-    current_date = datetime.datetime.now()
-    valid_temperature = TemperatureLog(
-        date=current_date, temperature=200, action="Heat"
-    )
-    return valid_temperature
 
 
 # Tests
@@ -76,26 +39,3 @@ def test_environment_variables():
     assert main_obj.TOKEN == expected_token
     assert main_obj.T_MAX == expected_t_max
     assert main_obj.T_MIN == expected_t_min
-
-
-class TestTempoeratureLog:
-    """Tests for the TemperatureLog model and its interactions."""
-
-    def test_send_event_to_database(self, db_session):
-        """
-        Tests if the event data is correctly sent and stored in the database.
-        """
-        # Add a new temperature log entry to the session
-        current_date = datetime.datetime.now()
-        temperature = 200
-        action = "Heat"
-        temperature_log_entry = TemperatureLog(
-            date=current_date, temperature=temperature, action=action
-        )
-        db_session.add(temperature_log_entry)
-
-        log = (
-            db_session.query(TemperatureLog).filter_by(temperature=temperature).first()
-        )
-        assert log.temperature == 200
-        assert log.action == "Heat"
